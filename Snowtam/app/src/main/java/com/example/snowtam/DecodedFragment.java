0a3aa2cd76434b32f010332865d72b74ad1ebe1b
GhadaRV <ghada.dridi@esprit.tn>
2020-12-09 18:03:44 +0100

    tabs

---
+++ Snowtam/app/src/main/java/com/example/snowtam/DecodedFragment.java
@@ -0,0 +1,45 @@
+package com.example.snowtam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DecodedFragment extends Fragment {
+    private String searchFor;
+    public static TextView decodedTV ;
+
+    FetchDataSnowTamDecoded fetchedDecoded=new FetchDataSnowTamDecoded();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_decoded, container, false);
+
+
+       /*FetchDataSnowTam process = new FetchDataSnowTam();
+        process.setCodeSnowTam(this.searchFor);
+        process.execute();*/
+       // String airportName = getArguments().getString("airportName");
+        //String fetchedSnowtam = getArguments().getString("fetchedSnowtam");
+      //  String fetchedSnowtam = process.textCode;
+
+        decodedTV = view.findViewById(R.id.decoded);
+       // decodedSnowtam=fetchedDecoded.SnowtamDecoded(""+fetchedSnowtam,"Airport: ");
+     // decodedTV.setText(decodedSnowtam);
+      // decodedTV.setText(decodedSnowtam);
+
+       // decodedTV.setText(decodedSnowtam);
+        return view;
+    }
+
+ /*   public void setSearchFor(String searchFor) {
+        this.searchFor = searchFor;
+    }*/
+}
---
+++ Snowtam/app/src/main/java/com/example/snowtam/OriginalFragment.java
@@ -0,0 +1,68 @@
+package com.example.snowtam;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link OriginalFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OriginalFragment extends Fragment {
+    public static TextView tv ;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public OriginalFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment OriginalFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static OriginalFragment newInstance(String param1, String param2) {
+        OriginalFragment fragment = new OriginalFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+        tv = view.findViewById(R.id.text_view);
+        return view;
+    }
+}
